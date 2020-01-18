7c78ef52e1afd7b07253892f19256cf217ca15b6
nciganovic <nciganovic99@gmail.com>
2020-01-18 11:00:53 +0100

    working on starting page

---
+++ js/main.js
@@ -0,0 +1,69 @@
+var nav = document.getElementsByTagName('nav')[0];
+
+if(window.innerWidth > 992){
+    nav.classList.remove('dark-nav');
+    nav.classList.add('transp-nav');
+
+    var nav = document.getElementsByTagName('nav')[0];
+        if(window.scrollY > 5){
+            nav.classList.remove('transp-nav');
+            nav.classList.add('dark-nav');
+        }
+        else{
+            nav.classList.add('transp-nav');
+            nav.classList.remove('dark-nav');
+        }
+        window.addEventListener('scroll', function(e){
+            //console.log(window.scrollY);
+            if(window.innerWidth > 992){
+                if(window.scrollY > 5){
+                    nav.classList.remove('transp-nav');
+                    nav.classList.add('dark-nav');
+                }
+                else{
+                    nav.classList.add('transp-nav');
+                    nav.classList.remove('dark-nav');
+                }
+            }
+        })
+}
+else{
+    nav.classList.add('dark-nav');
+    nav.classList.remove('transp-nav');
+}
+
+        
+window.onresize = function(event) {
+    if(window.innerWidth > 992){
+        console.log(window.innerWidth);
+        var nav = document.getElementsByTagName('nav')[0];
+        if(window.scrollY > 5){
+            nav.classList.remove('transp-nav');
+            nav.classList.add('dark-nav');
+        }
+        else{
+            nav.classList.add('transp-nav');
+            nav.classList.remove('dark-nav');
+        }
+        window.addEventListener('scroll', function(e){
+            //console.log(window.scrollY);
+            if(window.innerWidth > 992){
+                if(window.scrollY > 5){
+                    nav.classList.remove('transp-nav');
+                    nav.classList.add('dark-nav');
+                }
+                else{
+                    nav.classList.add('transp-nav');
+                    nav.classList.remove('dark-nav');
+                }
+            }
+        })
+    
+    } 
+    else{
+        var nav = document.getElementsByTagName('nav')[0];
+        nav.classList.add('dark-nav');
+        nav.classList.remove('transp-nav');
+    }    
+};
+
