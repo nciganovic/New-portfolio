6ae92098f8458384579fd0ec65b7a3667c3a5be0
nciganovic <nciganovic99@gmail.com>
2020-01-27 22:52:50 +0100

    fixed small bugs and added all links

--- js/main.js
+++ js/main.js
@@ -84,37 +84,43 @@ $(document).ready(function(){
         title: "CLOTHYY",
         tech: ["HTML 5", "CSS 3", "Javascript | Jquery library", "Python 3.7 | Django Framework", "MySQL", "Linux Server", "Apache 2"],
         desc: "Contains features like shopping cart, user shopping history, searching for specific product, stripe integration.",
-        img:"clothyy-final.mp4"
+        img:"clothyy-final.mp4",
+        src:"https://github.com/nciganovic/Clothy"
     },
     {
         title: "Email Developer",
         tech: ["HTML 5", "CSS 3", "Javascript"],
         desc: "Simple HTML and CSS landing page for selling online courses i made in 2 days as a youtube challenge. ",
-        img:"htmlemail-final.mp4"
+        img:"htmlemail-final.mp4",
+        src:"https://github.com/nciganovic/LandingPage"
     },
     {
         title: "IMPERIAL",
         tech: ["HTML 5", "CSS 3", "Javascript | Jquery"],
         desc: "Imperial is restaurant website. First website i made that has Javascript and Jquery implemented. Also has regex for form validation.",
-        img:"imperial-fianl.mp4"
+        img:"imperial-fianl.mp4",
+        src:"https://github.com/nciganovic/Restaurant"
     },
     {
         title: "Read And Write",
         tech: ["HTML 5", "CSS 3", "Python 3.7 | Django"],
         desc: "Read and Write is a website for bloggers to write their blogs. Users can create, read, update, delete their blogs. Users can also view their stats, like and comment.",
-        img:"randw-final.mp4"
+        img:"randw-final.mp4",
+        src:"https://github.com/nciganovic/Blog"
     },
     {
         title: "Amazon REST API",
         tech: ["Python 3.7", "Django REST Framework", "Beautiful Soup 4"],
         desc: "REST API for amazon.com, let's you get information about certain products. Database is automaticly filled using script with Beautiful Soup",
-        img:"rest-final.mp4"
+        img:"rest-final.mp4",
+        src:"https://github.com/nciganovic/DjangoRestApp"
     },
     {
         title: "LEARN.PY",
         tech: ["HTML 5", "CSS 3", "Bootstap 4"],
         desc: "Mobile friendly website for selling online Python courses.",
-        img:"learnpy-final.mp4"
+        img:"learnpy-final.mp4",
+        src:"https://github.com/nciganovic/Learn.py-website"
     },
     ]
 
@@ -133,7 +139,8 @@ $(document).ready(function(){
             </video>
         `)
 
-
+        $('.souce-btn').attr('href', item.src);
+        
         //<video autoplay controls loop muted id="video" class="w-100 border">
         //<source src="video/${item.img}" type="video/mp4">
         //</video>
